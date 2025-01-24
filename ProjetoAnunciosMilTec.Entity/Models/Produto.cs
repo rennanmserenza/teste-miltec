e82449806bdc3f2e2dd55afb7614ecfe29b38ddf
Rennan <Rennan@DESKTOP-UM7G5NH>
2025-01-23 23:46:42 -0400

    [UPDATE] Limpeza projetos, inicialização na criação das entidades que comporão nosso banco de dados.

---
+++ ProjetoAnunciosMilTec.Entity/Models/Produto.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoAnunciosMilTec.Entity.Models
+{
+    public class Produto : Anuncio
+    {
+        [Required(ErrorMessage = "Selecione uma categoria válida.")]
+        public string Categoria { get; set; }
+
+        [Required(ErrorMessage = "Selecione um modelo válido.")]
+        public string Modelo { get; set; }
+
+        [Required(ErrorMessage = "Selecione uma condição válida para o produto.")]
+        public string Condicao { get; set; }
+
+        [Required(ErrorMessage = "A quantidade deve ser no mínimo 1 unidade.")]
+        [Range(1, int.MaxValue, ErrorMessage = "A quantidade deve ser no mínimo 1 unidade.")]
+        public int Quantidade { get; set; }
+    }
+}
